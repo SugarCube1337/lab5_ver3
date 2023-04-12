c2f43afeba2d95bb31d1590b0c0a7c035a75d4ad
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-04-12 13:46:08 +0300

    Updated Main class

--- src/main/java/org/lab5/Main.java
+++ src/main/java/org/lab5/Main.java
@@ -1,7 +1,22 @@
 package org.lab5;
 
+import org.lab5.data.Route;
+import org.lab5.utility.Loader;
+
+import java.util.HashSet;
+
 public class Main {
+    private static HashSet<Route> routes = new HashSet<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 1) {
+            System.out.println("Необходимо указать имя файла в формате JSON");
+            return;
+        }
+        String filename = args[0];
+        Loader<HashSet<Route>, Route> loader = new Loader<>();
+
+        loader.loadFromJSON(routes, filename);
     }
 }
+
