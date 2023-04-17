3330ffc25f6950133724a461180ab46ca95e1d98
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-04-17 22:54:49 +0300

    update Tasks class

--- src/main/java/org/lab5/task/Tasks.java
+++ src/main/java/org/lab5/task/Tasks.java
@@ -43,5 +43,68 @@ public class Tasks {
     }
 
 
+    /**
+     * Requests long from inputManager
+     * @param inputManager handles the input
+     * @return provided long
+     * @throws NumberFormatException occurs when the input is incorrect
+     */
+    public static long getLong(InputManager inputManager) throws NumberFormatException {
+        var line = getString(inputManager);
+        return Long.parseLong(line);
+    }
+
+    /**
+     * Requests float from inputManager
+     * @param inputManager handles the input
+     * @return provided float
+     * @throws NumberFormatException occurs when the input is incorrect
+     */
+    public static float getFloat(InputManager inputManager) throws NumberFormatException {
+        var line = getString(inputManager);
+        return Float.parseFloat(line);
+    }
+
+    /**
+     * Requests double from inputManager
+     * @param inputManager handles the input
+     * @return provided double
+     * @throws NumberFormatException occurs when the input is incorrect
+     */
+    public static double getDouble(InputManager inputManager) throws NumberFormatException {
+        var line = getString(inputManager);
+        return Double.parseDouble(line);
+    }
+
+    /**
+     * Requests coordinates using inputManager
+     * @param inputManager handles the input
+     * @return provided coordinates
+     */
+    public static Coordinates getCoordinates(InputManager inputManager) {
+        Double x = null;
+        Integer y = null;
+        while (true) {
+            try {
+                if (x == null) {
+                    System.out.print("Specify X value for coordinates (Double): ");
+                    x = getDouble(inputManager);
+                }
+                if (y == null) {
+                    System.out.print("Specify Y value for coordinates (Integer): ");
+                    y = getInteger(inputManager);
+                }
+                return new Coordinates(x, y);
+            } catch (IllegalFieldException ex) {
+                System.out.println(ex.getMessage());
+                x = null;
+                y = null;
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Incorrect input format");
+            }
+        }
+    }
+
+
 
 
