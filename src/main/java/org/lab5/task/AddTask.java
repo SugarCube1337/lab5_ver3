1ad186431a893ec464dd9b179f3ef20f6f1607b1
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-04-17 14:33:48 +0300

    add Tasks class

---
+++ src/main/java/org/lab5/task/AddTask.java
@@ -0,0 +1,27 @@
+package org.lab5.task;
+
+public class AddTask implements Task {
+
+    private final InputManager inputManager;
+
+    public AddTask(InputManager inputManager) {
+        this.inputManager = inputManager;
+    }
+
+    @Override
+    public void execute() {
+        while (true) {
+            var route = Tasks.getRoute(inputManager);
+            System.out.println("You are about to add route:");
+            System.out.println(route);
+            if (Tasks.getApproval("Proceed", inputManager)) {
+                CollectionManager.add(route);
+                System.out.println("Successfully added element");
+                return;
+            }
+            if (!Tasks.getApproval("Do you want to refill element with another values", inputManager)) {
+                return;
+            }
+        }
+    }
+}
---
+++ src/main/java/org/lab5/task/Tasks.java
@@ -0,0 +1,9 @@
+package org.lab5.task;
+
+
+/**
+ * Class that contains some util methods for task
+ */
+public class Tasks {
+
+    private Tasks() {}
