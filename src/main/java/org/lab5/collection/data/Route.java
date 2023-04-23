53d4687106051db2afd9d33ed69a815ad90ef102
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-04-23 21:26:42 +0300

    update Route  class

--- src/main/java/org/lab5/collection/data/Route.java
+++ src/main/java/org/lab5/collection/data/Route.java
@@ -1,77 +1,116 @@
 package org.lab5.collection.data;
 
+import org.lab5.collection.CollectionManager;
+import org.lab5.exception.IllegalFieldException;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Represents a route with a unique ID, name, coordinates, creation date, start and end locations, and distance.
  */
 public final class Route implements Comparable<Route>, Serializable {
-    private long id; // The value of this field must be greater than 0, The value of this field must be unique, The value of this field must be generated automatically
-    private String name; // The field cannot be null, the string cannot be empty
-    private Coordinates coordinates; // The field can't be null
-    private java.time.LocalDateTime creationDate; // The value of this field must be generated automatically
-    private Location from; // The field can't be null
-    private Location to; // The field cannot be null
-    private int distance; // The value of this field must be greater than 1
+    private static final long serialVersionUID = 6701593067860758465L;
+
+    private long id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
+    private LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    private Location.From from; //Поле не может быть null
+    private Location.To to; //Поле не может быть null
+    private int distance; //Значение поля должно быть больше 1
 
     public static final long MIN_ID = 0;
     public static final int MIN_DISTANCE = 1;
 
-    private Route() {
-        // Private constructor
+    public Route() {}
+
+    public Route(String name,
+                 Coordinates coordinates,
+                 Location.From from,
+                 Location.To to,
+                 int distance) {
+        setId(CollectionManager.genId());
+        setName(name);
+        setCoordinates(coordinates);
+        setCreationDate(LocalDateTime.now());
+        setFrom(from);
+        setTo(to);
+        setDistance(distance);
     }
 
     public void setId(long id) {
+        if (id < MIN_ID) {
+            throw new IllegalFieldException("id", this);
+        }
         this.id = id;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalFieldException("name", this);
+        }
+        this.name = name;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        if (coordinates == null) {
+            throw new IllegalFieldException("coordinates", this);
+        }
+        this.coordinates = coordinates;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        if (creationDate == null) {
+            throw new IllegalFieldException("creationDate", this);
+        }
+        this.creationDate = creationDate;
+    }
+
+    public void setFrom(Location.From from) {
+        if (from == null) {
+            throw new IllegalFieldException("from", this);
+        }
+        this.from = from;
+    }
+
+    public void setTo(Location.To to) {
+        if (to == null) {
+            throw new IllegalFieldException("to", this);
+        }
+        this.to = to;
+    }
+
+    public void setDistance(int distance) {
+        if (distance < MIN_DISTANCE) {
+            throw new IllegalFieldException("distance", this);
+        }
+        this.distance = distance;
     }
 
     public long getId() {
         return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setCoordinates(Coordinates coordinates) {
-        this.coordinates = coordinates;
     }
 
     public Coordinates getCoordinates() {
         return coordinates;
     }
 
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
     public LocalDateTime getCreationDate() {
         return creationDate;
     }
 
-    public void setFrom(Location from) {
-        this.from = from;
-    }
-
-    public Location getFrom() {
+    public Location.From getFrom() {
         return from;
     }
 
-    public void setTo(Location to) {
-        this.to = to;
-    }
-
-    public Location getTo() {
+    public Location.To getTo() {
         return to;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
     }
 
     public int getDistance() {
@@ -79,69 +118,38 @@ public final class Route implements Comparable<Route>, Serializable {
     }
 
     @Override
-    public int compareTo(Route o) {
-        return Integer.compare(this.distance, o.distance);
-    }
-
-    @Override
     public String toString() {
-        return "Route{" +
+        return "Route[" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", coordinates=" + coordinates +
-                ", creationDate=" + creationDate +
+                ", creationDate=" + creationDate.format(DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy")) +
                 ", from=" + from +
                 ", to=" + to +
                 ", distance=" + distance +
-                '}';
+                ']';
     }
 
-    public static RouteBuilder newBuilder() {
-        return new Route().new RouteBuilder();
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
     }
 
-    public final class RouteBuilder {
-        private RouteBuilder() {
-            // private constructor
-        }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Route route)) return false;
+        return id == route.id
+                && distance == route.distance
+                && name.equals(route.name)
+                && coordinates.equals(route.coordinates)
+                && creationDate.equals(route.creationDate)
+                && from.equals(route.from)
+                && to.equals(route.to);
+    }
 
-        public RouteBuilder setId(long id) {
-            Route.this.id = id;
-            return this;
-        }
-
-        public RouteBuilder setName(String name) {
-            Route.this.name = name;
-            return this;
-        }
-
-        public RouteBuilder setCoordinates(Coordinates coordinates) {
-            Route.this.coordinates = coordinates;
-            return this;
-        }
-
-        public RouteBuilder setCreationDate(LocalDateTime creationDate) {
-            Route.this.creationDate = creationDate;
-            return this;
-        }
-
-        public RouteBuilder setFrom(Location from) {
-            Route.this.from = from;
-            return this;
-        }
-
-        public RouteBuilder setTo(Location to) {
-            Route.this.to = to;
-            return this;
-        }
-
-        public RouteBuilder setDistance(int distance) {
-            Route.this.distance = distance;
-            return this;
-        }
-
-        public Route build() {
-            return Route.this;
-        }
+    @Override
+    public int compareTo(Route route) {
+        return this.getDistance() - route.getDistance();
     }
 }
