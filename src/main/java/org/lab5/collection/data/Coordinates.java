3b7b934764fadd8f13b740f8d6aff1ca54b43ca1
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-04-26 11:34:05 +0300

    update Coordinates class

--- src/main/java/org/lab5/collection/data/Coordinates.java
+++ src/main/java/org/lab5/collection/data/Coordinates.java
@@ -1,18 +1,37 @@
 package org.lab5.collection.data;
 
+import org.lab5.exception.IllegalFieldException;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  * A class representing a pair of coordinates (x, y).
  */
-public class Coordinates {
-    private Double x; //The field cannot be null
+public class Coordinates implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -5296510993918124324L;
+
+    private Double x; //Поле не может быть null
     private int y;
 
+    public Coordinates() {}
 
     public Coordinates(Double x, int y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public void setX(Double x) {
         if (x == null) {
-            throw new IllegalArgumentException("x coordinate cannot be null");
+            throw new IllegalFieldException("x", this);
         }
         this.x = x;
+    }
+
+    public void setY(int y) {
         this.y = y;
     }
 
@@ -20,18 +39,27 @@ public class Coordinates {
         return x;
     }
 
-    public void setX(Double x) {
-        if (x == null) {
-            throw new IllegalArgumentException("x coordinate cannot be null");
-        }
-        this.x = x;
-    }
-
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    @Override
+    public String toString() {
+        return "Coordinates[" +
+                "x=" + x +
+                ", y=" + y +
+                ']';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Coordinates that)) return false;
+        return getY() == that.getY() && getX().equals(that.getX());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
     }
 }
