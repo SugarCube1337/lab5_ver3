f195194713223a7938114ba57573f5686076f76c
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-06-01 01:28:29 +0300

    add CollectionLoader class + update Main class

--- src/main/java/org/lab5/Lab5.java
+++ src/main/java/org/lab5/Lab5.java
@@ -1,25 +1,16 @@
 package org.lab5;
 
 
-import org.lab5.collection.CollectionManager;
 import org.lab5.exception.AbortException;
-import org.lab5.exception.IllegalFieldException;
 import org.lab5.parser.BaseCommandParser;
 import org.lab5.parser.CommandParser;
 import org.lab5.parser.ConsoleInputManager;
-import java.io.IOException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFilePermissions;
+
 
 /**
  * Main class for lab 5
  */
 
-import java.io.IOException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class Lab5 {
 
@@ -28,50 +19,29 @@ public class Lab5 {
 
     /**
      * Entry point for the project
+     *
      * @param args contains information about collection's data
      */
     public static void main(String[] args) {
-
         if (args.length > 0) {
             path = args[0];
         } else {
             System.err.println("Path to data is not provided, using default: " + DEFAULT_PATH);
         }
 
-        try {
-            try {
-                CollectionManager.load(path);
-            } catch (JsonMappingException ex) {
-                if (ex.getMessage().contains("No content to map due to end-of-input")) {
-                    System.err.println("Error: JSON file is empty or incomplete");
-                } else {
-                    throw ex;
-                }
-            } catch (IOException ex) {
-                System.err.println("Error: Unable to load collection - " + ex.getMessage());
-            }
-        } catch (IllegalFieldException ex) {
-            System.err.println("Unable to load collection: " + ex.getMessage());
-        } catch (SecurityException ex) {
-            System.err.println("Error: Permission denied while loading data. Please check your access rights.");
-        } catch (Throwable ex) {
-            System.err.println("Critical error");
-            ex.printStackTrace();
-        }
+        CollectionLoader.loadCollection(path);
 
-        final CommandParser PARSER = new BaseCommandParser();
-        try (ConsoleInputManager inputManager = new ConsoleInputManager(PARSER)) {
+        final CommandParser parser = new BaseCommandParser();
+        try (ConsoleInputManager inputManager = new ConsoleInputManager(parser)) {
             inputManager.start();
         } catch (AbortException ex) {
             System.out.println("Operation aborted, stopping the program");
-        } catch (Throwable ex) {
-            System.err.println("Critical error");
-            ex.printStackTrace();
         }
     }
 
     /**
      * Returns path to file where data is located
+     *
      * @return path to file with data
      */
     public static String getPath() {
